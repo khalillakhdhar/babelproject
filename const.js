6e5f93191e533d87583116d986d836ccba891b15
khalil <khalillakhdharatc@gmail.com>
2022-05-31 11:01:47 +0100

    constdemo

---
+++ const.js
@@ -0,0 +1,6 @@
+let a =1;
+if (a == 1) {
+const age = 10;
+//console.log(age);
+}
+console.log(age);
---
+++ const_es5.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var a = 1;
+if (a == 1) {
+  var _age = 10;
+  //console.log(age);
+}
+console.log(age);
