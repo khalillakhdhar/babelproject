4530163b548c28971da1e6afcadc7fbd3a4875c3
khalil <khalillakhdharatc@gmail.com>
2022-05-31 09:31:38 +0100

    commit1

---
+++ index.js
@@ -0,0 +1,4 @@
+var arrowfunction =() =>{
+    return "hello world";
+
+}
---
+++ index_es5.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var arrowfunction = function arrowfunction() {
+    console.log("hello world")
+    return "hello world";
+
+};
