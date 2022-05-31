0e31b7b53e0872827fb3b27ade4b92845ec6d6a3
khalil <khalillakhdharatc@gmail.com>
2022-05-31 11:14:09 +0100

    class&arrows

---
+++ arrowfunction.js
@@ -0,0 +1,5 @@
+var add = (x,y) => {
+    return x+y;
+    }
+    var k = add(3,6);
+    console.log(k);
---
+++ arrowfunction_es5.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var add = function add(x, y) {
+    return x + y;
+};
+var k = add(3, 6);
+console.log(k);
